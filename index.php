668b6b773d243b4b38413af1920ec5c47f0e248d
CodeElevator <98259474+CodeElevator@users.noreply.github.com>
2022-03-05 16:36:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php phpinfo(); ?>
+<!DOCTYPE html>
+<!--- Basic html ---!>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Disbot blog!</title>
+  </head>
+  <body>
+    Text
+  </body>
+</html>
